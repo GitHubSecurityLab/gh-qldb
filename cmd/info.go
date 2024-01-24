170b1d9cd7e8a26d322f70c17c041282efbe6e14
Alvaro Muñoz <pwntester@github.com>
2024-01-24 11:56:40 +0100

    Enable info command to work with paths or nwos

--- cmd/info.go
+++ cmd/info.go
@@ -17,7 +17,7 @@ var infoCmd = &cobra.Command{
 	Short: "Returns information about a database stored in the QLDB structure",
 	Long:  `Returns information about a database stored in the QLDB structure`,
 	Run: func(cmd *cobra.Command, args []string) {
-		info(nwoFlag, languageFlag)
+		info()
 	},
 }
 
@@ -26,58 +26,90 @@ func init() {
 	infoCmd.Flags().StringVarP(&nwoFlag, "nwo", "n", "", "The NWO of the repository to get the database for.")
 	infoCmd.Flags().StringVarP(&languageFlag, "language", "l", "", "The primary language you want the database for.")
 	infoCmd.Flags().BoolVarP(&jsonFlag, "json", "j", false, "Use json as the output format.")
-	infoCmd.MarkFlagRequired("nwo")
-	infoCmd.MarkFlagRequired("language")
+	infoCmd.Flags().StringVarP(&dbPathFlag, "db-path", "p", "", "Path to the database to get the info from.")
+	infoCmd.MarkFlagsOneRequired("db-path", "nwo")
+	infoCmd.MarkFlagsMutuallyExclusive("db-path", "nwo")
 }
 
-func info(nwo string, language string) {
-	dir := utils.GetPath(nwo)
-	entries, err := os.ReadDir(dir)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var pathList []map[string]string
-	for _, e := range entries {
-		entryName := e.Name()
-		var name string
-		if filepath.Ext(entryName) == ".zip" {
-			// remove the .zip extension if it exists
-			name = entryName[:len(entryName)-len(filepath.Ext(entryName))]
-		} else if e.IsDir() {
-			name = e.Name()
-		} else {
-			continue
-		}
+func info() {
+	var results []map[string]string
 
-		// split the name by the "-". first element is the language, second is the short commit sha
-		nameSplit := strings.Split(name, "-")
-		if len(nameSplit) != 2 {
-			log.Fatal(fmt.Errorf("invalid database name: %s", name))
-		}
-		if nameSplit[0] != language {
-			continue
-		}
-		shortSha := nameSplit[1]
-
-		commitSha, committedDate, err := utils.GetCommitInfo2(nwo, shortSha)
-		if err != nil {
-			log.Fatal(err)
-		}
-		pathList = append(pathList, map[string]string{
-			"commitSha":     commitSha,
-			"committedDate": committedDate,
-			"path":          filepath.Join(dir, entryName),
-		})
+	if nwoFlag != "" {
+		results = infoFromNwo(nwoFlag)
+	} else if dbPathFlag != "" {
+		results = append(results, infoFromPath(dbPathFlag))
 	}
 	if jsonFlag {
-		jsonStr, err := json.MarshalIndent(pathList, "", "  ")
+		jsonStr, err := json.MarshalIndent(results, "", "  ")
 		if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Printf("%s", jsonStr)
 	} else {
-		for _, path := range pathList {
-			fmt.Println(path["path"])
+		for _, result := range results {
+			fmt.Println(result["path"])
 		}
 	}
+}
+
+func infoFromPath(path string) map[string]string {
+	// get the file name part of path
+	parts := strings.Split(path, string(os.PathSeparator))
+	name := parts[len(parts)-1]
+	base := filepath.Dir(path)
+
+	var dbname string
+	if fi, err := os.Stat(path); err == nil && fi.IsDir() {
+		dbname = path
+	} else if filepath.Ext(name) == ".zip" {
+		dbname = name[:len(name)-len(filepath.Ext(name))]
+	} else {
+		log.Fatal(fmt.Errorf("invalid database path: %s", path))
+		return nil
+	}
+
+	// split the name by the "-". first element is the language, second is the short commit sha
+	nameSplit := strings.Split(dbname, "-")
+	if len(nameSplit) != 2 {
+		log.Fatal(fmt.Errorf("invalid database name: %s", name))
+		return nil
+	}
+
+	lang := nameSplit[0]
+	shortSha := nameSplit[1]
+	baseParts := strings.Split(base, string(os.PathSeparator))
+	nwo := filepath.Join(baseParts[len(baseParts)-2], baseParts[len(baseParts)-1])
+	commitSha, committedDate, err := utils.GetCommitInfo2(nwo, shortSha)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return map[string]string{
+		"commitSha":     commitSha,
+		"committedDate": committedDate,
+		"language":      lang,
+		"path":          path,
+	}
+}
+
+func infoFromNwo(nwo string) []map[string]string {
+	dir := utils.GetPath(nwo)
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var pathList []string
+
+	for _, e := range entries {
+		entryName := e.Name()
+		if filepath.Ext(entryName) == ".zip" || e.IsDir() {
+			pathList = append(pathList, filepath.Join(dir, entryName))
+		}
+	}
+
+	var results []map[string]string
+	for _, path := range pathList {
+		results = append(results, infoFromPath(path))
+	}
+	return results
 }
--- go.mod
+++ go.mod
@@ -4,27 +4,31 @@ go 1.19
 
 require (
 	github.com/cli/go-gh v1.2.1
-	github.com/shurcooL/githubv4 v0.0.0-20230424031643-6cea62ecd5a9
-	github.com/spf13/cobra v1.7.0
+	github.com/shurcooL/githubv4 v0.0.0-20240120211514-18a1ae0e79dc
+	github.com/spf13/cobra v1.8.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/cli/safeexec v1.0.0 // indirect
-	github.com/cli/shurcooL-graphql v0.0.2 // indirect
-	github.com/henvic/httpretty v0.0.6 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/cli/safeexec v1.0.1 // indirect
+	github.com/cli/shurcooL-graphql v0.0.4 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/henvic/httpretty v0.1.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
-	github.com/mattn/go-runewidth v0.0.13 // indirect
-	github.com/muesli/termenv v0.12.0 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/shurcooL/graphql v0.0.0-20220606043923-3cf50f8a0a29 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/shurcooL/graphql v0.0.0-20230722043721-ed46e5a46466 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/thlib/go-timezone-local v0.0.0-20210907160436-ef149e42d28e // indirect
-	golang.org/x/net v0.9.0 // indirect
-	golang.org/x/oauth2 v0.7.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
-	golang.org/x/term v0.7.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/oauth2 v0.16.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/term v0.16.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
